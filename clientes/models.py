399befe9f5ddaed3eb0635659ee1ed4adb698b8c
xoyoc <xoyoc_l2@hotmail.com>
2025-01-28 19:15:41 -0600

    Inicio del proyecto de Diego

---
+++ clientes/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class ClientesModel(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Nombre del Cliente')
+    rfc = models.CharField(max_length=15, verbose_name='RFC')
